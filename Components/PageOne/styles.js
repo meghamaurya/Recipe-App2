e97492ed94f920c5f6bbdcd572beb1c34b02f276
megha <meghamini40@gmail.com>
2022-12-27 15:51:05 +0530

    extra styles

---
+++ Components/PageOne/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    font300: {
+        fontFamily: 'KumbhSans_300Light'
+    },
+    font400: {
+        fontFamily: 'KumbhSans_400Regular'
+    },
+    font700: {
+        fontFamily: 'KumbhSans_700Bold',
+    }
+})
