a63d4043259456027fa27e3107264633cc6bdfde
megha <meghamini40@gmail.com>
2023-01-03 17:48:12 +0530

    changed icons display

--- Components/Page1/IconsBar.jsx
+++ Components/Page1/IconsBar.jsx
@@ -13,16 +13,15 @@ const IconsBar = (props) => {
   } else {
     const handlePress = () => {
       props.setIconClick(props.title);
-      setBgChange(true);
     };
     return (
       <TouchableHighlight
         underlayColor={"yellow"}
-        style={s`my-4 py-3 mx-1.5 px-0.25  w-1/6 rounded-3xl`}
+        style={s`my-4 py-3 mx-1.5 px-0.25 justify-evenly rounded-3xl`}
         onPress={handlePress}
       >
         <View style={s`items-center text-gray-800`}>
-          <props.iconName size={40} />
+          <props.iconName style={s`text-5xl`} />
           <Text style={[s`items-center text-md my-1 `, styles.font600]}>
             {props.title}
           </Text>
