f9e3da1e9f281c682f353768b0d289e8d1c52ae6
megha <meghamini40@gmail.com>
2023-01-04 15:15:56 +0530

    fixed getting apikey issue

--- Components/Page1/Cards.jsx
+++ Components/Page1/Cards.jsx
@@ -1,27 +1,58 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 import { s } from "react-native-wind";
+import {
+  useFonts,
+  KumbhSans_500Medium,
+  KumbhSans_700Bold,
+} from "@expo-google-fonts/kumbh-sans";
+import styles from "./styles";
 const Cards = (props) => {
   const details = props.data;
   console.log(details, "cards");
-
-  return (
-    <View>
-      <Text>Title</Text>
-      <View style={s`flex-1 flex-row w-full flex-wrap`}>
-        {props.data.map((item) => (
-          <View key={item.id} style={s`m-3`}>
-            <Image
-              source={item.image}
-              style={s`w-40 h-40  border rounded-md`}
-            />
-            {/* <Text style={s`text-lg`}>{item.title}</Text> */}
-            {/* <Text>{item.time}</Text> */}
-          </View>
-        ))}
+  let [fontsLoaded] = useFonts({
+    KumbhSans_500Medium,
+    KumbhSans_700Bold,
+  });
+  if (!fontsLoaded) {
+    return null;
+  } else {
+    return (
+      <View style={s`m-2  `}>
+        <Text
+          style={[s`mx-4 text-3xl text-yellow-500 capitalize`, styles.font700]}
+        >
+          {props.iconClick}
+        </Text>
+        <View style={s`flex-1 flex-row w-full flex-wrap justify-around`}>
+          {props.error ? (
+            <>
+              <Text style={s`my-10 font-semibold text-2xl text-rose-500`}>
+                Oops, Recipe not available
+              </Text>
+            </>
+          ) : (
+            <>
+              {props.data.map((item) => (
+                <View key={item.id} style={s`m-3  `}>
+                  <View style={s`flex-1 flex-cols w-40  flex-wrap `}>
+                    <Image
+                      source={item.image}
+                      style={s`w-full h-48 resize-cover rounded-3xl`}
+                    />
+                    <Text style={[s`text-lg flexShrink-1`, styles.font600]}>
+                      {item.title}
+                    </Text>
+                    <Text>{item.readyInMinutes} min</Text>
+                  </View>
+                </View>
+              ))}
+            </>
+          )}
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 };
 
 export default Cards;
