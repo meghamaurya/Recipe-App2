cb9aee31a173baa494b69701d94c5c230d8171d5
megha <meghamini40@gmail.com>
2023-01-05 12:02:53 +0530

    added activeindicator and fix time

--- Components/Page1/Cards.jsx
+++ Components/Page1/Cards.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  Image,
+  ActivityIndicator,
+} from "react-native";
 import { s } from "react-native-wind";
 import {
   useFonts,
@@ -7,13 +13,16 @@ import {
   KumbhSans_700Bold,
 } from "@expo-google-fonts/kumbh-sans";
 import styles from "./styles";
-const Cards = (props) => {
+const Cards = (props, { navigation }) => {
   const details = props.data;
   console.log(details, "cards");
   let [fontsLoaded] = useFonts({
     KumbhSans_500Medium,
     KumbhSans_700Bold,
   });
+  const handleCards = () => {
+    console.log("clicked");
+  };
   if (!fontsLoaded) {
     return null;
   } else {
@@ -24,32 +33,55 @@ const Cards = (props) => {
         >
           {props.iconClick}
         </Text>
-        <View style={s`flex-1 flex-row w-full flex-wrap justify-around`}>
-          {props.error ? (
-            <>
-              <Text style={s`my-10 font-semibold text-2xl text-rose-500`}>
-                Oops, Recipe not available
-              </Text>
-            </>
-          ) : (
-            <>
-              {props.data.map((item) => (
-                <View key={item.id} style={s`m-3  `}>
-                  <View style={s`flex-1 flex-cols w-40  flex-wrap `}>
-                    <Image
-                      source={item.image}
-                      style={s`w-full h-48 resize-cover rounded-3xl`}
-                    />
-                    <Text style={[s`text-lg flexShrink-1`, styles.font600]}>
-                      {item.title}
-                    </Text>
-                    <Text>{item.readyInMinutes} min</Text>
-                  </View>
-                </View>
-              ))}
-            </>
-          )}
-        </View>
+        {props.loading ? (
+          <ActivityIndicator size="large" color="#eab32d" style={s`my-10`} />
+        ) : (
+          <>
+            <View style={s`flex-1 flex-row w-full flex-wrap justify-around`}>
+              {props.error ? (
+                <>
+                  <Text style={s`my-10 font-semibold text-2xl text-rose-500`}>
+                    Oops, Recipe not available
+                  </Text>
+                </>
+              ) : (
+                <>
+                  {props.data.map((item) => (
+                    <TouchableHighlight
+                      key={item.id}
+                      style={s`m-3`}
+                      underlayColor={"transparent"}
+                      // onPress={() => {
+                      //   navigation.navigate("Details", {
+                      //     itemId: item.id,
+                      //   }
+                      //   );
+                      // }}
+                    >
+                      <View style={s`flex-1 flex-cols w-40  flex-wrap `}>
+                        <Image
+                          source={item.image}
+                          style={s`w-full h-48 resize-cover rounded-3xl`}
+                        />
+                        <Text style={[s`text-lg`, styles.font600]}>
+                          {item.title}
+                        </Text>
+                        <Text
+                          style={[
+                            s`absolute top-4 right-2 text-white rounded-lg px-1 py-0.25`,
+                            styles.time,
+                          ]}
+                        >
+                          {item.readyInMinutes} min
+                        </Text>
+                      </View>
+                    </TouchableHighlight>
+                  ))}
+                </>
+              )}
+            </View>
+          </>
+        )}
       </View>
     );
   }
