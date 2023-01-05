305c29a5ba7cd927ad5842e7814bc52f30121b1d
Rahul Rajput <rahulrajput21112@gmail.com>
2023-01-05 08:26:10 +0530

    Fixed conflicts

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
+<<<<<<< HEAD
 import Page2 from './Components/Page2/Page2';
+=======
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import PageOne from './Components/Page1/PageOne';
+>>>>>>> megha
 
 export default function App({navigation}) {
   return (
+<<<<<<< HEAD
+    <Page2  />
+  );
+}
+=======
     <View style={styles.container}>
       <PageOne />
       {/* <Text style={s`text-red-800`}>Starting New Native Projecsft</Text>
@@ -21,3 +29,4 @@ const styles = StyleSheet.create({
     // justifyContent: 'center',
   },
 });
+>>>>>>> megha
