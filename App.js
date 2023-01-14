c872c9b77f28cd1ebd76329e3146f2323f8ade78
Rahul Rajput <rahulrajput21112@gmail.com>
2023-01-14 18:09:48 +0530

    Updated

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
+import React, {useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import PageOne from './Components/Page1/PageOne';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Page2 from './Components/Page2/Page2'
+import { REACT_APP_API_KEY } from "@env"
 const Stack = createNativeStackNavigator();
 
+
+
 export default function App() {
+  useEffect(() => {
+    console.log(REACT_APP_API_KEY)
+  }, [])
   return (
     <NavigationContainer >
       <Stack.Navigator initialRouteName="Home" screenOptions={{
