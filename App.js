b38b6f1e4895d283248348ff0a20b28ad5964889
megha <meghamini40@gmail.com>
2023-01-18 14:09:15 +0530

    installed expo constants

--- App.js
+++ App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import PageOne from './Components/Page1/PageOne';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Page2 from './Components/Page2/Page2'
+import Page2 from './Components/Page2/Page2';
 const Stack = createNativeStackNavigator();
 
-
-
 export default function App() {
-  
   return (
     <NavigationContainer >
       <Stack.Navigator initialRouteName="Home" screenOptions={{
